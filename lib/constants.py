0fd69b9a0ac49aff43ac4a105c472e205843e59e
Antoni Zajko <antoni.zajko.1@gmail.com>
2022-10-22 22:02:49 +0200

    Merge with rebase

--- lib/constants.py
+++ lib/constants.py
@@ -11,29 +11,33 @@ PARSED_PDFS_FRAME_COLUMNS = ["filename", "raw_text", "intro"]
 PARSED_PDFS_FRAME_COLUMNS.extend(SECTIONS_NAMES)
 
 SPLIT_TOKEN = "<SPLIT_TOKEN>"
+TEAM_NAME = "Pomysłowi_Inżynierowie_Wielkich_Okazji_KIID_META.csv"
 
 DATA_PATH = Path("data")
-RAW_KIIDS_DIR = Path("data/raw")
-PARSED_KIIDS_DIR = Path("data/parsed")
+RAW_KIIDS_DIR = DATA_PATH / "raw"
+PARSED_KIIDS_DIR = DATA_PATH / "parsed"
 TEMPORARY_PATH = Path("temp")
+FINAL_DATA_DIR = DATA_PATH / "final"
+META_FILE_PATH = FINAL_DATA_DIR / (TEAM_NAME + ".csv")
+
 
 KEY_PHRASES_DICT = {
-    'Kluczowe informacje dla inwestorów': 'intro',
-    'Niniejszy dokument zawiera kluczowe informacje dla inwestorów dotyczące tego funduszu. Nie są to materiały marketingowe. Dostarczenie tych informacji jest wymogiem prawnym mającym na celu ułatwienie zrozumienia charakteru i ryzyka związanego z inwestowaniem w ten fundusz. Przeczytanie niniejszego dokumentu jest zalecane inwestorowi, aby mógł on podjąć świadomą decyzję inwestycyjną.' : 'intro',
-    'Niniejsze kluczowe informacje dla inwestorów są aktualne na dzień': 'informacje praktyczne',
-    'Fundusz otrzymał zezwolenie na prowadzenie działalności w': 'raw_text',
-    'Zalecenie: niniejszy fundusz może nie być odpowiedni dla inwestorów, którzy planują wycofać swoje środki w ciągu': 'cele i polityka inwestycyjna',
-    'może zostać pociągnięta do odpowiedzialności za każde oświadczenie zawarte w niniejszym dokumencie, które wprowadza w błąd, jest niezgodne ze stanem faktycznym lub niespójne z odpowiednimi częściami prospektu emisyjnego UCITS.': 'informacje praktyczne',
-    'Opłaty jednorazowe pobierane przed lub po dokonaniu inwestycji': 'opłaty',
-    'Opłata za subskrypcję': 'raw_text',
-    'Opłata za umorzenie': 'opłaty',
-    'Opłaty pobierane z funduszu w ciągu roku': 'opłaty',
-    'Opłaty bieżące': 'opłaty',
-    'Opłaty pobierane z funduszu w określonych warunkach szczególnych': 'raw_text',
-    'Opłata za wyniki': 'opłaty',
-    'Cele i polityka inwestycyjna': 'cele i polityka inwestycyjna',
-    'Profil ryzyka i zysku': 'profil ryzyka i zysku',
-    'Opłaty': 'opłaty',
-    'Wyniki osiągnięte w przeszłości': 'wyniki osiągnięte w przeszłości',
-    'Informacje praktyczne': 'informacje praktyczne	'
-    }
+    "Kluczowe informacje dla inwestorów": "intro",
+    "Niniejszy dokument zawiera kluczowe informacje dla inwestorów dotyczące tego funduszu. Nie są to materiały marketingowe. Dostarczenie tych informacji jest wymogiem prawnym mającym na celu ułatwienie zrozumienia charakteru i ryzyka związanego z inwestowaniem w ten fundusz. Przeczytanie niniejszego dokumentu jest zalecane inwestorowi, aby mógł on podjąć świadomą decyzję inwestycyjną.": "intro",
+    "Niniejsze kluczowe informacje dla inwestorów są aktualne na dzień": "informacje praktyczne",
+    "Fundusz otrzymał zezwolenie na prowadzenie działalności w": "raw_text",
+    "Zalecenie: niniejszy fundusz może nie być odpowiedni dla inwestorów, którzy planują wycofać swoje środki w ciągu": "cele i polityka inwestycyjna",
+    "może zostać pociągnięta do odpowiedzialności za każde oświadczenie zawarte w niniejszym dokumencie, które wprowadza w błąd, jest niezgodne ze stanem faktycznym lub niespójne z odpowiednimi częściami prospektu emisyjnego UCITS.": "informacje praktyczne",
+    "Opłaty jednorazowe pobierane przed lub po dokonaniu inwestycji": "opłaty",
+    "Opłata za subskrypcję": "raw_text",
+    "Opłata za umorzenie": "opłaty",
+    "Opłaty pobierane z funduszu w ciągu roku": "opłaty",
+    "Opłaty bieżące": "opłaty",
+    "Opłaty pobierane z funduszu w określonych warunkach szczególnych": "raw_text",
+    "Opłata za wyniki": "opłaty",
+    "Cele i polityka inwestycyjna": "cele i polityka inwestycyjna",
+    "Profil ryzyka i zysku": "profil ryzyka i zysku",
+    "Opłaty": "opłaty",
+    "Wyniki osiągnięte w przeszłości": "wyniki osiągnięte w przeszłości",
+    "Informacje praktyczne": "informacje praktyczne	",
+}
--- lib/get_fund_info.py
+++ lib/get_fund_info.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import re
 
-def retrive_register_id(knf_df: 'pd.DataFrame', intro_text: str) -> int:
-    
-    does_not_id_exist = knf_df['nr_w_rejestrze'].apply(lambda register_id: re.search(str(register_id) , intro_text)).isna()
-    df_filtered = knf_df[~ does_not_id_exist]
 
-    if len(df_filtered['nr_w_rejestrze'].unique()) != 1:
+def retrive_register_id(knf_df: "pd.DataFrame", intro_text: str) -> int:
+
+    does_not_id_exist = (
+        knf_df["nr_w_rejestrze"]
+        .apply(lambda register_id: re.search(str(register_id), intro_text))
+        .isna()
+    )
+    df_filtered = knf_df[~does_not_id_exist]
+
+    if len(df_filtered["nr_w_rejestrze"].unique()) != 1:
         return None
-    
-    return df_filtered['nr_w_rejestrze'].iloc[0]
 
-def retrive_fund_name(knf_df: 'pd.DataFrame', intro_text: str, register_id: int = None) -> str:
+    return df_filtered["nr_w_rejestrze"].iloc[0]
+
+
+def retrive_fund_name(
+    knf_df: "pd.DataFrame", intro_text: str, register_id: int = None
+) -> str:
 
     if register_id is None:
         return None
 
-    df_filtered = knf_df[knf_df['nr_w_rejestrze'] == register_id]
-    full_fund_name = df_filtered['fundusz'].unique()[0]
+    df_filtered = knf_df[knf_df["nr_w_rejestrze"] == register_id]
+    full_fund_name = df_filtered["fundusz"].unique()[0]
 
     return full_fund_name
-   
+
+
 def retrive_subfund_name(knf_df: "pd.DataFrame", intro_text: str) -> str:
 
     does_not_name_exist = (
@@ -30,9 +39,6 @@ def retrive_subfund_name(knf_df: "pd.DataFrame", intro_text: str) -> str:
     )
     df_filtered = knf_df[~does_not_name_exist]
 
-    if df_filtered.size == 0:
-        return None
-
     fund_name = (
         df_filtered["subfundusz"]
         .sort_values(key=lambda x: x.str.len(), ascending=False)
@@ -41,57 +47,41 @@ def retrive_subfund_name(knf_df: "pd.DataFrame", intro_text: str) -> str:
 
     return fund_name
 
-def retrive_organisation_name(knf_df: 'pd.DataFrame', intro_text: str, register_id: int = None) -> str:
+
+def retrive_organisation_name(
+    knf_df: "pd.DataFrame", intro_text: str, register_id: int = None
+) -> str:
 
     if register_id is None:
         return None
 
-    df_filtered = knf_df[knf_df['nr_w_rejestrze'] == register_id]
-    full_organisation_name = df_filtered['towarzystwo'].unique()[0]
-    
+    df_filtered = knf_df[knf_df["nr_w_rejestrze"] == register_id]
+    full_organisation_name = df_filtered["towarzystwo"].unique()[0]
+
     return full_organisation_name
 
-def retrive_fund_country_id(knf_df: 'pd.DataFrame', intro_text: str):
 
-    does_not_id_exist = (
-        knf_df["indentyfikator_krajowy"]
-        .apply(lambda register_id: re.search(str(register_id), intro_text))
-        .isna()
-    )
-    df_filtered = knf_df[~does_not_id_exist]
-
-    if df_filtered.shape[0] != 1:
-        return None
-
-    return df_filtered['indentyfikator_krajowy'].iloc[0]
-
-def retrive_organisation_id(knf_df: 'pd.DataFrame', intro_text: str):
-
-    does_not_id_exist = (
-        knf_df["identyfikator_krajowy_funduszu"]
-        .apply(lambda register_id: re.search(str(register_id), intro_text))
-        .isna()
-    )
-    df_filtered = knf_df[~does_not_id_exist]
-
-    if df_filtered.shape[0] != 1:
-        return None
-
-    return df_filtered['indentyfikator_krajowy'].iloc[0]
-
-def retive_all_info(knf_df: 'pd.DataFrame', intro_text: str, register_id: int = None) -> str:
+def retive_all_info(
+    knf_df: "pd.DataFrame", intro_text: str, register_id: int = None
+) -> str:
     register_id = retrive_register_id(knf_df, intro_text)
     fund_name = retrive_fund_name(knf_df, intro_text, register_id)
     subfund_name = retrive_subfund_name(knf_df, intro_text)
-    organisation_name = retrive_organisation_name(knf_df, intro_text, register_id)
-    fund_country_id = retrive_fund_country_id(knf_df, intro_text)
-    organisation_country_id = retrive_organisation_id(knf_df, intro_text)
+    organisation_name = retrive_organisation_name(
+        knf_df, intro_text, register_id
+    )
 
     return {
-        'fund_name': fund_name,
-        'subfund_name': subfund_name,
-        'organisation_name': organisation_name,
-        'register_id': register_id,
-        'fund_country_id': fund_country_id,
-        'organisation_country_id': organisation_country_id
+        "fund_name": fund_name,
+        "subfund_name": subfund_name,
+        "organisation_name": organisation_name,
+        "register_id": register_id,
     }
+
+
+def main(df_knf_path: str = "./../data/knf_funds.pkl"):
+    knf_df = pd.read_pickle(df_knf_path)
+
+
+if __name__ == "__main__":
+    main()
