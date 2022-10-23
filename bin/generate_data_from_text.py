237a3c995cc851ff9a3e9bc660c691862eb01bfc
DawidPludowski <dawid.pludowski@gmail.com>
2022-10-23 04:51:35 +0200

    add row functions to pipeline

--- bin/generate_data_from_text.py
+++ bin/generate_data_from_text.py
@@ -6,7 +6,23 @@ from typing import Dict, Callable
 import pandas as pd
 
 from lib.extract_text_data.srri import extract_srri
-from lib.extract_text_data.funds import retrive_register_id, retrive_fund_name, retrive_subfund_name, retrive_organisation_name, retrive_fund_country_id
+from lib.extract_text_data.funds import (retrive_register_id, 
+                                         retrive_fund_name,
+                                         retrive_subfund_name,
+                                         retrive_organisation_name,
+                                         retrive_fund_country_id)
+
+from lib.extract_text_data.pratical_info import (
+    get_depositary,
+    get_krs,
+    get_nip,
+    get_address,
+    get_capital,
+    get_currency,
+    get_esg,
+    get_type
+)
+
 from lib.constants import (
     PARSED_KIIDS_DIR,
     GENERATED_COLUMNS,
@@ -20,7 +36,15 @@ COLS_TO_GENERATING_FUNCTIONS_MAPPING: Dict[str, Callable] = {
     "NAZWA_FUNDUSZU": retrive_fund_name,
     "IDENTYFIKATOR_KRAJOWY": retrive_fund_country_id,
     "NUMER_RFI": retrive_register_id,
-    "PODMIOT_ZARZADZAJACY": retrive_organisation_name
+    "PODMIOT_ZARZADZAJACY": retrive_organisation_name,
+    "DEPOZYTARIUSZ": get_depositary,
+    "KRS_TOWARZYSTWA": get_krs,
+    "NIP_TOWARZYSTWA": get_nip,
+    "SZIEDZIBA_TOWARZYSTWA": get_address,
+    "KAPITAL_ZAKLADOWY_TOWARZYSTWA": get_capital
+    "WALUTA_KAPITALU_ZAKLADOWEGO_TOWARZYSTWA": get_currency,
+    "CZY_ESG": get_esg,
+    "TYP_FUNDUSZU": get_type
 }
 
 
---
+++ lib/extract_text_data/pratical_info.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import re
+
+def split_text(text: str) -> list:
+    text = text.replace('\n\n', '<p>')
+    text = ' '.join(text.split())
+    text = text.replace('<p>', '\n')
+    # text = text.lower()
+    return text
+
+def get_depo(text: str) -> str or None:
+    regex = depo_regex.search(text)
+    if regex is None:
+        return None
+    else:     
+        if not_depo_regex.search(regex.group(2)):
+            return None
+        
+        if regex.group(3) is not None:
+            return regex.group(2) + regex.group(3)
+        else:
+            return regex.group(2)
+
+def get_depositary(row) -> str:
+    
+    text = row['informacje praktyczne']
+    
+    text = split_text(text)
+        
+    return get_depo(text)
+    
+def get_krs(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)   
+    
+    krs_regex = re.compile('(krs:?\s*)([0-9]{9})', re.IGNORECASE)
+    regex = krs_regex.search(text)
+    if regex is None:
+        return None
+    else:
+        return regex.group(2)
+    
+def get_nip(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)   
+    
+    nip_regex = re.compile('(nip:?\s*)([0-9\-]*)(\s|\.|,)', re.IGNORECASE)   
+    regex = nip_regex.search(text)
+    if regex is None:
+        return None
+    else:
+        return regex.group(2) 
+    
+def get_address(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)   
+    
+    address_regex = re.compile('(ul\.\s\w+\s\d{1,3},?\s\d{2}-\d{3}\s\w+)')
+    
+    regex = address_regex.search(text)
+    if regex is None:
+        return None
+    else:
+        return regex.group(1)    
+    
+def get_capital(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)      
+    
+    capital_regex = re.compile('zakładowy:?.{,20}([\d\s,]+)', re.IGNORECASE)
+    
+    regex = capital_regex.search(text)
+    
+    if regex is None:
+        return None
+    else:
+        return regex.group(1)
+    
+def get_currency(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)      
+    
+    currency_regex = re.compile('(przedstawiono\sw\s)(\w{3})', re.IGNORECASE)
+    
+    regex = currency_regex.search(text)
+    if regex is None:
+        return None
+    else:
+        return regex.group(2)     
+    
+def get_esg(row) -> str:
+    text = row['informacje praktyczne']
+    text = split_text(text)   
+    
+    esg_regex = re.compile('esg' , re.IGNORECASE)
+    not_esg_regex = re.compile('.{0,10}nie.{0,10}esg.{0,10}nie.{0,10}')  
+    
+    regex = esg_regex.search(text)
+    
+    if regex is None:
+        return False
+    else:
+        not_regex = not_esg_regex.search(text)
+        if not_regex is None:
+            return False
+        return True
+    
+def get_type(row) -> str:
+    text = row['raw_text']
+    text = split_text(text)  
+    
+    type_regex = re.compile('(S?FIO)')
+    
+    regex = type_regex.search(text)
+    
+    if regex is None:
+        return None
+    else:
+        return regex.group(1) 
+    
